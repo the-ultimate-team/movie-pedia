ecbc3033de7381619536c8ddff1e5794e50416e3
eunhasublue <eunhasublooming@gmail.com>
2022-02-26 15:52:21 +0900

    fix: 임시 route

---
+++ pages/Nav.js
@@ -0,0 +1,9 @@
+import React from "react";
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from "@emotion/react";
+
+const NavBar = () => {
+  return <div>nav</div>;
+};
+
+export default NavBar;
