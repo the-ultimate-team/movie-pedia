7a15b60de7872844b226ebf092d93b6b7f60d229
jiyoungbkim <jiyoungbkim@gmail.com>
2022-02-26 15:38:36 +0900

    add: logo image

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,1 +1,14 @@
-test;
+import React from "react";
+import { css, jsx } from "@emotion/react";
+import Image from "next/image";
+import logo from "../assets/Logo.png";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Image src={logo} />
+    </div>
+  );
+};
+
+export default Navbar;
