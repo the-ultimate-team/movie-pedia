dd3b3654d1b6258d9fdcedef25d4c5f83f14ff75
jiyoungbkim <jiyoungbkim@gmail.com>
2022-02-26 16:41:30 +0900

    add: nomalize.css

--- pages/Nav.js
+++ pages/Nav.js
@@ -1,9 +1,10 @@
 import React from "react";
 /** @jsxImportSource @emotion/react */
 import { css, jsx } from "@emotion/react";
+import Navbar from "../components/Navbar";
 
-const NavBar = () => {
-  return <div>nav</div>;
+const Nav = () => {
+  return <Navbar />;
 };
 
-export default NavBar;
+export default Nav;
