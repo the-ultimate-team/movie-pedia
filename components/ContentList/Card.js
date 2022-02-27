4572708c17efe3b18b40bdea46d48bb788218edb
eunhasublue <eunhasublooming@gmail.com>
2022-02-27 19:46:37 +0900

    feat: Movie API Get

--- components/ContentList/Card.js
+++ components/ContentList/Card.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 /** @jsxImportSource @emotion/react */
 import { css, jsx } from "@emotion/react";
+import Image from "next/image";
+import axios from "axios";
 
 const CardStyle = css`
   display: flex;
@@ -38,10 +40,28 @@ const AvgGradeStyle = css`
   line-height: 14px;
 `;
 
+const API_KEY = "10923b261ba94d897ac6b81148314a3f";
+
 const Card = () => {
+  const [moives, setMovies] = useState([]);
+
+  useEffect(async () => {
+    const res = await axios
+      .get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+      .then((res) => {
+        setMovies(res.data);
+        console.log(res.data);
+      });
+  }, []);
+
   return (
     <div css={CardStyle}>
-      <div css={ImageStyle}>이미지</div>
+      <img
+        src="https://image.tmdb.org/t/p/w500/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg"
+        css={ImageStyle}
+        alt="image"
+        layout="fill"
+      />
       <div css={TitleStyle}>타이틀</div>
       <div css={YearNationSort}>
         <div css={YearNationStyle}>년도&nbsp;</div>
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
