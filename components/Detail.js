8c11d5b8981f2b42ea50fd7f4d7f251c08150e34
eunhasublue <eunhasublooming@gmail.com>
2022-03-04 16:45:12 +0900

    feat: Detail Page Header 구상

--- components/Detail.js
+++ components/Detail.js
@@ -3,7 +3,14 @@ import React from "react";
 import { css, jsx } from "@emotion/react";
 
 const Detail = () => {
-  return <div></div>;
+  return (
+    <div>
+      <section>
+        <div>대형 이미지</div>
+        <div>소형 포스터</div>
+      </section>
+    </div>
+  );
 };
 
 export default Detail;
