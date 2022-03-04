1c65ae28ae3c5599de42f6339d018e7e1dd5d78c
eunhasublue <eunhasublooming@gmail.com>
2022-03-04 16:41:25 +0900

    Detail Page Create

---
+++ components/Detail.js
@@ -0,0 +1,9 @@
+import React from "react";
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from "@emotion/react";
+
+const Detail = () => {
+  return <div></div>;
+};
+
+export default Detail;
